bfb41c99981768f101142dfd7808ae751d0107b5
Serg Tikhomirov <tihomirowiko@gmail.com>
2018-05-18 01:09:50 +0300

    serv test

--- serv.c
+++ serv.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstring>
 #include <stdlib.h>
+
+void error(const String str){
+  perror(str);
+  exit(0);
+}
 
 int main(int argc, char** argv){
   int value = 0;
@@ -11,6 +17,42 @@ int main(int argc, char** argv){
   else if (argc == 2)
   {
     value = atoi(argv[1]);
+  }
+
+  struct sockaddr_in addr;
+  int listenFd, commFd;
+  String buffs = "";
+  char buff[100];
+  size_t res = 0;//result of different funcs
+
+  listenFd = socket(AF_INET, SOCK_STREAM, 0);
+  bzero(&addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htons(INADDR_ANY);
+  addr.sin_port = htons(22000);
+  
+  bind(listenFd, (struct sockaddr *) addr, sizeof(addr));
+  listen(listenFd, 10);
+
+  commFd = accept(listenFd, (struct sockaddr *) addr, sizeof(addr));
+  while(1)
+  {
+    buffs = "";
+    bzero(buff, 100);
+    do {
+      res =  recv(commFd, buff, 100, MSG_PEEK);
+      if (res > 0) {
+	buffs += buff;
+	memset(buff, 0, 100);
+      }else if(res == 0){
+      }else{
+	error("recv");
+      }
+    }while(res < 100);
+    std::cout << buffs << endl;
+    res = send(commFd, buffs, strlen(buffs), NULL);
+    if (res < 0)
+      error("send");
   }
   
   printf("hello world %d\n", value);
