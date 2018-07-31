a24a309929255e4aaee16c4f6af11b5d0b35cba8
Serg Tikhomirov <tihomirowiko@gmail.com>
2018-07-31 22:51:35 +0300

    bag fix there was removed first recv

--- serv.c
+++ serv.c
@@ -28,7 +28,7 @@ void print(std::string str, T temp){
 }
 
 int main(int argc, char** argv){
-  int portNum = 0;
+  int portNum = 22000;
   if (argc > 2)
   {
     printf("too many parameters\n");
@@ -45,6 +45,7 @@ int main(int argc, char** argv){
   std::set<User> servUser;
   struct sockaddr_in addr;
   long listenFd, commFd;
+  long maxFd;
   std::string buffs = "";
   char buff[100];
   size_t res = 0;//result of different funcs
@@ -52,14 +53,13 @@ int main(int argc, char** argv){
   int on = 1;
   
   listenFd = socket(AF_INET, SOCK_STREAM, 0);
-  //fcntl(listenFd, F_SETFL, O_NONBLOCK);
   setsockopt(listenFd, SOL_SOCKET,  SO_REUSEADDR,
                    (char *)&on, sizeof(on));
   ioctl(listenFd, FIONBIO, (char *)&on);
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = htons(INADDR_ANY);
-  addr.sin_port = htons(22000);
+  addr.sin_port = htons(portNum);
   
   res = bind(listenFd, (struct sockaddr *) &addr, sizeof(addr));
   if (res < 0) error("bind", errno);
@@ -79,70 +79,47 @@ int main(int argc, char** argv){
     timeout.tv_sec = 60;
 
     FD_ZERO(&readSock);
-    FD_SET(listenFd, &readSock); 
-    /* for(std::set<long>::iterator i = servSocket.begin(); i != servSocket.end(); i++){ */
-    /*   FD_SET(*i, &readSock); */
-    /* } */
+    FD_SET(listenFd, &readSock);
+    maxFd = listenFd;
 
-    for(std::set<User>::iterator i = servUser.begin(); i != servUser.end(); ++i)
-      {
-	FD_SET(i->GetFD(), &readSock);
-      }
+    for(std::set<User>::iterator i = servUser.begin(); i != servUser.end(); ++i) {
+      FD_SET(i->GetFD(), &readSock);
+      if(maxFd < i->GetFD()) maxFd = i->GetFD();
+    }
 
-    
-    countSock = select(listenFd+servSocket.size()+1, &readSock, NULL, NULL, &timeout);
-    if(countSock == 0)
+    countSock = select(maxFd+1, &readSock, NULL, NULL, &timeout);
+    if(countSock == 0){    //timeout
+      print("timeout: select is ", countSock);
       break;
-    if(countSock < 0)
-      error("select", errno);
+    }
+    if(countSock < 0) error("select", errno);
 
     if(FD_ISSET(listenFd, &readSock)){
       commFd = accept(listenFd, (struct sockaddr *)&addr, &sizeAddr);
       if(commFd < 0)
 	error("accept", errno);
-      else if(servSocket.insert(commFd).second){
-	//fcntl(commFd, F_SETFL, O_NONBLOCK);
+      else if(servUser.insert(User(commFd, "name", addr)).second){
 	ioctl(commFd, FIONBIO, (char *)&on);
-	servSocket.insert(commFd);
-	User name(commFd, "name", addr);
-	servUser.insert(name);
+	User nameTemp(commFd, "name", addr);
+	servUser.insert(nameTemp);
 	print("connect", commFd);
 	--countSock;
-	do {
-	  res =  recv(commFd, buff, 100, 0);
-	  if (res > 0) {
-	    buffs += buff;
-	    memset(buff, 0, 100);
-	    std::cout << "serv " << res << std::endl;
-	  }else if(res == 0){
-	    close(commFd);
-	    servSocket.erase(commFd);
-	  }else{
-	    error("recv", errno);
-	  }
-	}while(res == 100);
-	if (buffs != "")
-	  std::cout << "Serv: " << buffs << std::endl;
-	res = send(commFd, buffs.c_str(), buffs.size(), 0);
-	if (res < 0)
-	  error("send", errno);
       }
     }
-    
 
-    
-    for(std::set<long>::iterator i = servSocket.begin(); i != servSocket.end(); ++i){
-      if(FD_ISSET(*i, &readSock)){
-	commFd = *i;
+    for(std::set<User>::iterator i = servUser.begin(); i != servUser.end(); ++i){
+      if(FD_ISSET(i->GetFD(), &readSock)){
+	commFd = i->GetFD();
 	do {
 	  res =  recv(commFd, buff, 100, 0);
 	  if (res > 0) {
 	    buffs += buff;
 	    memset(buff, 0, 100);
-	    std::cout << "serv " << res << std::endl;
 	  }else if(res == 0){
 	    close(commFd);
-	    servSocket.erase(commFd);
+	    print("disconnect ", commFd);
+	    i->Show();
+	    servUser.erase(*i);
 	  }else{
 	    error("recv", errno);
 	  }
@@ -154,10 +131,9 @@ int main(int argc, char** argv){
 	  error("send", errno);
       }
     }
-    //sleep(1);
   }
   
   
-  printf("hello world %d\n", portNum);
+  printf("Bye world %d\n", portNum);
   return 0;
 }
